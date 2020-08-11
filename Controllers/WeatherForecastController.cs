fe35500c5cbbccda2b0d79b187764110b79d48a4
Alberto <hiunkeru@gmail.com>
2020-08-11 14:55:05 +0200

    prueba commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EjemploGitHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public void GetPrueba()
+        {
+
+            var foo = 1;
+            switch (foo)
+            {
+                               case 0: Console.Write("1");
+           break;
+                      case 1:
+                                                Console.Write("1");
+                    break;
+           case 2:
+                                        Console.Write("1");
+                    break;
+                case 3:
+           Console.Write("1");
+                    Console.Write("2");
+                    break;
+          default:
+                    break;
+            }
+        }
+    }
+}
